69124ecf30bd2847608c8d0682fd3af4df9284cb
Igor Trifonov <yorherim@gmail.com>
2023-03-25 16:41:45 +0800

    add fonts, create components Stopwatch & Button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,72 @@
-<template>test</template>
+<template>
+  <div class="container">
+    <div class="stop-watchers">
+      <Stopwatch />
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+import Stopwatch from "@/components/Stopwatch.vue";
+
+export default {
+  components: { Stopwatch },
+  data() {
+    return {
+      stopWatchers: [
+        { id: 1, currentTime: Date.now() },
+        { id: 2, currentTime: Date.now() },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: "Gotham Pro";
+  src: url("@/fonts/GothamPro.woff") format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+:root {
+  --active: #ffffff;
+  --disable: #9e9e9e;
+  --dark: #353638;
+  --gray: #696969;
+}
+
+body {
+  font-family: "Gotham Pro", sans-serif;
+  background: var(--dark);
+}
+
+.container {
+  width: 775px;
+  margin: 0 auto;
+}
+
+.stop-watchers {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 45px 50px;
+  padding: 70px 0;
+}
+
+.active {
+  color: var(--active);
+  background: var(--active);
+}
+
+.disable {
+  color: var(--disable);
+  background: var(--disable);
 }
 </style>
---
+++ src/components/Stopwatch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="stopwatch">
+    <div class="stopwatch__time stopwatch__time_active">0:00</div>
+    <div class="stopwatch__divider stopwatch__divider_active"></div>
+    <div class="stopwatch__buttons">
+      <Button type="pause" />
+      <Button type="pause" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "@/components/Button.vue";
+
+export default {
+  components: { Button },
+};
+</script>
+
+<style scoped>
+.stopwatch {
+  width: 225px;
+  height: 120px;
+  background: var(--gray);
+  padding: 22px 0 20px;
+}
+
+.stopwatch__time {
+  text-align: center;
+  font-size: 22px;
+  block-size: 17px;
+  line-height: 17px;
+}
+.stopwatch__time_active {
+  color: var(--active);
+}
+.stopwatch__time_disable {
+  color: var(--disable);
+}
+
+.stopwatch__divider {
+  height: 1px;
+  margin: 20px 0;
+}
+.stopwatch__divider_active {
+  background: var(--active);
+}
+.stopwatch__divider_disable {
+  background: var(--disable);
+}
+
+.stopwatch__buttons {
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+}
+</style>
